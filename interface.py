ca68f1a6f3c86478997b1a47a16aaf5fbe355d2e
GabsNandes <136932490+GabsNandes@users.noreply.github.com>
2023-06-18 01:05:24 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,35 +1,39 @@
 import socket
 import threading
 from logger import write_log
+from collections import Counter
+import numpy as np
 
 HOST = "localhost"
 PORT = 12345
 REQUEST_MESSAGE = "REQUEST"
 GRANTED_MESSAGE = "GRANTED"
 RELEASE_MESSAGE = "RELEASE"
+END_MESSAGE = "END"
 LOG_FILE_NAME = "interfaceLog.txt"
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-def start_interface():
+def start_interface(Q, fullfilled_processes):
+
+
     print("Selecione a ação desejada: ")
         
     option = input("1 - Imprimir fila atual de pedidos\n2 - Imprimir quantidade de vezes que cada processo foi atendido\n3 - Encerrar execução\n")
     
     if option == "1":
-        write_log(LOG_FILE_NAME, REQUEST_MESSAGE.encode())
-    
-        sock.sendto(REQUEST_MESSAGE.encode(), (HOST, PORT))
-        return True
+        print(Q)
         
     elif option == "2":
-        pass
+        
+        Keys = Counter(fullfilled_processes).keys()
+        Counts = Counter(fullfilled_processes).values()
+        print("Processo: ",Keys)
+        print("Contagem: ", Counts)
     
     elif option == "3":
-        write_log(LOG_FILE_NAME, REQUEST_MESSAGE.encode())
-        
-        sock.sendto(RELEASE_MESSAGE.encode(), (HOST, PORT))
-        return False
+        return END_MESSAGE
         
 
 
