4bc539810d9b3acf82750fd32b763135c5c09b01
GabsNandes <136932490+GabsNandes@users.noreply.github.com>
2023-06-18 12:47:18 -0300

    Update processos_exe.py

--- processos_exe.py
+++ processos_exe.py
@@ -7,11 +7,22 @@ def main():
     RUNNING = 0
     while RUNNING<3:
         thread1 = threading.Thread(target=cl.main(RUNNING))
-
+        thread2 = threading.Thread(target=cl.main(RUNNING))
+        thread3 = threading.Thread(target=cl.main(RUNNING))
+        
         thread1.start()
+
+        thread2.start()
+
+        thread3.start()
+        
         
         thread1.join()
         
+        thread2.join()
+
+        thread3.join()
+
         RUNNING +=1
           
     
